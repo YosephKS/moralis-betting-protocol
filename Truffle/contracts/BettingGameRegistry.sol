cd6a76255830c5787bc7e9ea2d40c86319143053
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-08 12:25:07 +0700

    adding some basic smart contracts

---
+++ Truffle/contracts/BettingGameRegistry.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.17 <0.9.0;
+
+contract BettingGameRegistry {}
---
+++ Truffle/contracts/BettingGameToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.17 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract BettingGameToken is ERC20, ERC20Burnable {
+    constructor(string _name, string _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 1 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
