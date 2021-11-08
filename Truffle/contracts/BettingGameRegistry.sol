2be310a7fc081a2f22f462361921caa505afb2ff
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-08 22:14:16 +0700

    adding more require & IERC20Burnable interface

--- Truffle/contracts/BettingGameRegistry.sol
+++ Truffle/contracts/BettingGameRegistry.sol
@@ -1,24 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.17 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "./IERC20Burnable.sol";
 
-contract BettingGameRegistry is Ownable {
+contract BettingGameRegistry is Ownable, ChainlinkClient {
+   using SafeMath for uint;
+
    enum BettingGameStatus { OPEN, CLOSED }
    struct BettingGame {
-      address owner;
+      address creator;
+      address challenger;
       uint256 sides;
       BettingGameStatus status;
       uint256 expiryTime;
    }
 
+   struct DepositBalance {
+      uint256 amount;
+      uint256 decimals;
+   }
+
    address public nativeTokenAddress;
    uint256 private bettingGameCount = 0;
-   mapping (uint256 => BettingGame) public bettingGameDataRegsiter;
+   mapping (uint256 => BettingGame) public bettingGameDataRegister;
+   mapping (address => mapping (address => DepositBalance)) public depositBalanceRegister;
 
    constructor(address _nativeTokenAddress) {
       nativeTokenAddress = _nativeTokenAddress;
+   }
+
+   /**
+    * Making sure that the `_bettingGameId` is valid
+    */
+   modifier onlyExistingGame(uint256 _bettingGameId) {
+      require(_bettingGameId <= bettingGameCount);
+      _;
    }
 
    function setNativeTokenAddress(address newNativeTokenAddress) public onlyOwner {
@@ -27,15 +46,32 @@ contract BettingGameRegistry is Ownable {
 
    function createGame(uint256 _sides) public {
       // 1. Burn some token
-      IERC20 nativeToken = IERC20(nativeTokenAddress);
+      IERC20Burnable nativeToken = IERC20Burnable(nativeTokenAddress);
+      nativeToken.burnFrom(msg.sender, SafeMath.mul(0.01 * 10 ** 18, _sides));
 
       // 2. Create new `BettingGame` instance
-      bettingGameDataRegsiter[bettingGameCount] = BettingGame(msg.sender, _sides, BettingGameStatus.OPEN, block.timestamp + 7 days);
+      bettingGameDataRegister[bettingGameCount] = BettingGame(msg.sender, address(0), _sides, BettingGameStatus.OPEN, block.timestamp + 7 days);
+
+      // 3. Increase Betting Game Counter
+      bettingGameCount = SafeMath.add(bettingGameCount, 1);
    }
 
-   function challengeGame() public {}
+   function challengeGame(uint256 _bettingGameId) public onlyExistingGame(_bettingGameId) {
+      require(block.timestamp < bettingGameDataRegister[_bettingGameId].expiryTime, "This game has expired!");
+      require(bettingGameDataRegister[_bettingGameId].creator != msg.sender, "You cannot challenge your own bet!");
+      // 1. Burn some token
+      IERC20Burnable nativeToken = IERC20Burnable(nativeTokenAddress);
+      uint256 sides = bettingGameDataRegister[_bettingGameId].sides;
+      nativeToken.burnFrom(msg.sender, SafeMath.mul(0.01 * 10 ** 18, sides));
 
-   function cancelGame() public {}
+      // 2. Assign Challenger to the Registry
+      bettingGameDataRegister[_bettingGameId].challenger = msg.sender;
+   }
+
+   function cancelGame(uint256 _bettingGameId) public onlyExistingGame(_bettingGameId) {
+      require(bettingGameDataRegister[_bettingGameId].creator == msg.sender, "You cancel a game you didn't create!");
+      bettingGameDataRegister[_bettingGameId].status = BettingGameStatus.CLOSED;
+   }
 
    function depositBettingAsset() public {}
 
--- Truffle/contracts/BettingGameToken.sol
+++ Truffle/contracts/BettingGameToken.sol
@@ -7,7 +7,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract BettingGameToken is ERC20, ERC20Burnable, Ownable {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(msg.sender, 1 * 10 ** decimals());
+    
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+    /**
+     * Public minting function (for testing purposes)
+     */
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
