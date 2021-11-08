57f14ce2702c5a6a007fcf97b88021b3f08d11dc
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-11-09 00:12:13 +0700

    finishing implementation for BettingGame & BettingGameRegistry without deposit and withdraw

--- Truffle/contracts/BettingGame.sol
+++ Truffle/contracts/BettingGame.sol
@@ -3,29 +3,82 @@ pragma solidity >=0.4.17 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC20Burnable.sol";
 
 contract BettingGame is Ownable, VRFConsumerBase {
+   enum BettingGameStatus { OPEN, CLOSED }
+
+   struct DepositBalance {
+      uint256 amount;
+      uint256 decimals;
+   }
+
    bytes32 internal keyHash;
    uint256 internal fee;
    uint256 public randomResult;
+   address public creator;
+   address public challenger;
+   uint256 public sides;
+   BettingGameStatus public status;
+   uint256 public expiryTime;
 
-   constructor(address _vrfCoordinatorAddress, address _linkTokenAddress, bytes32 _keyHash, uint256 _fee) 
+   mapping (bytes32 => address) requestIdToAddressRegistry;
+   mapping (address => uint256) playerBetRecordRegistry;
+   mapping (address => DepositBalance) public depositBalanceRegistry;
+
+   constructor(
+      address _vrfCoordinatorAddress,
+      address _linkTokenAddress,
+      bytes32 _keyHash,
+      uint256 _fee,
+      address _creator,
+      uint256 _sides,
+      uint256 _expiryTime
+   ) 
       VRFConsumerBase(_vrfCoordinatorAddress, _linkTokenAddress)
     {
         keyHash = _keyHash;
-        fee = _fee; // 0.1 LINK (Varies by network)
+        fee = _fee;
+        creator = _creator;
+        challenger = address(0);
+        sides = _sides;
+        status = BettingGameStatus.OPEN;
+        expiryTime = _expiryTime;
     }
+
+   /**
+    * Allow `_msgSend` address to register as a challenger
+    */
+    function challenge(address _msgSend) public onlyOwner {
+      challenger = _msgSend;
+    }
+
+   /**
+    *  Cancel the created Betting Game
+    */
+   function cancel() public onlyOwner {
+      status = BettingGameStatus.CLOSED;
+   }
+
+   /**
+    * Allow player `_msgSend` to place a bet on the game
+    */
+   function play(address _msgSend) public onlyOwner returns (bytes32 requestId) {
+      require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+      requestId = requestRandomness(keyHash, fee);
+      requestIdToAddressRegistry[requestId] = _msgSend;
+   }
+
+   /**
+    * This is a function overriden from Chainlink VRF contract to get the randomness result
+    */
+   function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+      address playerAddress = requestIdToAddressRegistry[requestId];
+      playerBetRecordRegistry[playerAddress] = randomness % sides;
+   }
 
    function deposit() public {}
 
-   function play() public returns (bytes32 requestId) {
-      require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
-      requestId = requestRandomness(keyHash, fee);
-   }
-
-   function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-      randomResult = randomness;
-   }
-
-   function cancel() public {}
+   function withdraw() public {}
 }
--- Truffle/contracts/BettingGameRegistry.sol
+++ Truffle/contracts/BettingGameRegistry.sol
@@ -4,32 +4,32 @@ pragma solidity >=0.4.17 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "./BettingGame.sol";
 import "./IERC20Burnable.sol";
 
 contract BettingGameRegistry is Ownable, ChainlinkClient {
    using SafeMath for uint;
 
-   enum BettingGameStatus { OPEN, CLOSED }
-   struct BettingGame {
-      address creator;
-      address challenger;
-      uint256 sides;
-      BettingGameStatus status;
-      uint256 expiryTime;
-   }
-
-   struct DepositBalance {
-      uint256 amount;
-      uint256 decimals;
-   }
-
    address public nativeTokenAddress;
+   address internal vrfCoordinatorAddress;
+   address internal linkTokenAddress;
+   bytes32 internal keyHash;
+   uint256 internal fee;
    uint256 private bettingGameCount = 0;
-   mapping (uint256 => BettingGame) public bettingGameDataRegister;
-   mapping (address => mapping (address => DepositBalance)) public depositBalanceRegister;
+   mapping (uint256 => address) public bettingGameDataRegistry;
 
-   constructor(address _nativeTokenAddress) {
+   constructor(
+      address _nativeTokenAddress,
+      address _vrfCoordinatorAddress,
+      address _linkTokenAddress,
+      bytes32 _keyHash,
+      uint256 _fee
+   ) {
       nativeTokenAddress = _nativeTokenAddress;
+      vrfCoordinatorAddress = _vrfCoordinatorAddress;
+      linkTokenAddress = _linkTokenAddress;
+      keyHash = _keyHash;
+      fee = _fee;
    }
 
    /**
@@ -49,31 +49,47 @@ contract BettingGameRegistry is Ownable, ChainlinkClient {
       IERC20Burnable nativeToken = IERC20Burnable(nativeTokenAddress);
       nativeToken.burnFrom(msg.sender, SafeMath.mul(0.01 * 10 ** 18, _sides));
 
-      // 2. Create new `BettingGame` instance
-      bettingGameDataRegister[bettingGameCount] = BettingGame(msg.sender, address(0), _sides, BettingGameStatus.OPEN, block.timestamp + 7 days);
+      // 2. Create new `BettingGame` smart contract
+      BettingGame newBettingGame = new BettingGame(
+         vrfCoordinatorAddress,
+         linkTokenAddress,
+         keyHash,
+         fee,
+         msg.sender,
+         _sides,
+         block.timestamp + 7 days
+      );
+      bettingGameDataRegistry[bettingGameCount] = address(newBettingGame);
 
       // 3. Increase Betting Game Counter
       bettingGameCount = SafeMath.add(bettingGameCount, 1);
    }
 
    function challengeGame(uint256 _bettingGameId) public onlyExistingGame(_bettingGameId) {
-      require(block.timestamp < bettingGameDataRegister[_bettingGameId].expiryTime, "This game has expired!");
-      require(bettingGameDataRegister[_bettingGameId].creator != msg.sender, "You cannot challenge your own bet!");
-      // 1. Burn some token
-      IERC20Burnable nativeToken = IERC20Burnable(nativeTokenAddress);
-      uint256 sides = bettingGameDataRegister[_bettingGameId].sides;
-      nativeToken.burnFrom(msg.sender, SafeMath.mul(0.01 * 10 ** 18, sides));
+      BettingGame existingGame = BettingGame(bettingGameDataRegistry[_bettingGameId]);
+      require(block.timestamp < existingGame.expiryTime(), "This game has expired!");
+      require(existingGame.creator() != msg.sender, "You cannot challenge your own bet!");
 
-      // 2. Assign Challenger to the Registry
-      bettingGameDataRegister[_bettingGameId].challenger = msg.sender;
+      // 1. Burn the token
+      IERC20Burnable nativeToken = IERC20Burnable(nativeTokenAddress);
+      nativeToken.burnFrom(msg.sender, SafeMath.mul(0.01 * 10 ** 18, existingGame.sides()));
+
+      // 2. Run the `challenge` function
+      existingGame.challenge(msg.sender);
    }
 
    function cancelGame(uint256 _bettingGameId) public onlyExistingGame(_bettingGameId) {
-      require(bettingGameDataRegister[_bettingGameId].creator == msg.sender, "You cancel a game you didn't create!");
-      bettingGameDataRegister[_bettingGameId].status = BettingGameStatus.CLOSED;
+      BettingGame existingGame = BettingGame(bettingGameDataRegistry[_bettingGameId]);
+      require(existingGame.creator() == msg.sender, "You cancel a game you didn't create!");
+
+      // Execute the internal `cancel` function
+      existingGame.cancel();
    }
+
+   function playGame() public {}
 
    function depositBettingAsset() public {}
 
-   function playGame() public {}
+   function withdrawBettingAsset() public {}
+
 }
