c8e098fb85d32010765eaaec24025e8a62357224
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2021-11-18 23:28:35 +0700

    minor fix on dashboard liveQuery

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useMemo } from "react";
-import { Typography, Row, Col } from "antd";
+import { Typography, Row, Col, Space } from "antd";
 import GameCard from "../components/GameCard";
 import GameModal from "components/GameModal";
 import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
 import { useMoralisQuery } from "react-moralis";
+import { useEffect } from "react/cjs/react.development";
 
 const styles = {
   wrapper: {
@@ -13,18 +14,20 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    width: "100%",
   },
 };
 
 export default function Dashboard() {
   const [visible, setVisible] = useState(false);
   const { walletAddress } = useMoralisDapp();
-  const { data } = useMoralisQuery(
+  const { data, fetch } = useMoralisQuery(
     "BettingGameCreatedKovan",
     (query) => query.notEqualTo("creator", walletAddress ?? ""),
     [],
     {
       live: true,
+      autoFetch: false,
     }
   );
 
@@ -35,6 +38,11 @@ export default function Dashboard() {
       return bettingGameAddress;
     });
   }, [data]);
+
+  useEffect(() => {
+    fetch();
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <>
@@ -49,13 +57,19 @@ export default function Dashboard() {
           </Typography.Text>
         </div>
         <Row gutter={16} style={{ marginTop: "2rem" }}>
-          {bettingGameData.map((address) => {
-            return (
-              <Col span={8}>
-                <GameCard cardTitle={address} />
-              </Col>
-            );
-          })}
+          {bettingGameData && bettingGameData.length > 0 ? (
+            bettingGameData.map((address) => {
+              return (
+                <Col span={8}>
+                  <GameCard cardTitle={address} />
+                </Col>
+              );
+            })
+          ) : (
+            <Space style={{ ...styles.title, fontSize: "18px" }}>
+              <Typography.Text>No Challenge Game Found.</Typography.Text>
+            </Space>
+          )}
         </Row>
       </div>
     </>
